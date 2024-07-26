64c4f7c766c80c8b294ea7cc43405b60a19e4a55
sakuraAndo <494912633@qq.com>
2024-07-26 17:47:42 +0800

    [main] : achieve all pages
    【提交描述】：完成账单页面和添加页面的搭建，完善ejs页面

--- tallyBook/routes/index.js
+++ tallyBook/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const nanoid = require('nanoid');
+const db = require('../db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/account', (req, res, next) => {
+  const lists = db.get('posts').value();
+  res.render('account', {lists: lists});
+});
+
+router.get('/create', (req, res, next) => {
+  res.render('create');
+});
+
+router.post('/create', (req, res, next) => {
+  db.get('posts')
+    .push({ id: nanoid(), ...req.body })
+    .write();
+  res.send('成功');
 });
 
 module.exports = router;
